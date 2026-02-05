c58089b6b93a187d2a2da8bed3e38a62fac27e09
Juniorzeh <juniorzeh206@gmail.com>
2026-02-05 13:17:45 -0300

    captura de eventos, criar novos itens, organizar elementos, data e hora, estilos dinÂmicos

---
+++ js/index.js
@@ -0,0 +1,48 @@
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+let contador = 0;
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === ""){
+        alert("Por favor insira um item!");
+        return;
+    }
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeitem = document.createElement("p");
+    nomeitem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeitem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR",{
+        weekday: "long"
+        
+    });
+
+    const data = new Date().toLocaleDateString("pt-Br");
+    const hora = new Date().toLocaleTimeString("pt-Br", {
+        hour: "numeric",
+        minute: "numeric"
+    });
+
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+
+    listaDeCompras.appendChild(itemDaLista);
+
+    
+})
